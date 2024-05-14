2caeb870dc2b594f9bdd3b4e8e595f095ca5bb6e
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-14 22:45:45 +0600

    email and create email file

---
+++ src/components/email/CreateEmail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateEmail = () => {
+  return (
+    <div>CreateEmail</div>
+  )
+}
+
+export default CreateEmail
---
+++ src/components/reporting/added/AddedRep.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddedRep = () => {
+  return (
+    <div>AddedRep</div>
+  )
+}
+
+export default AddedRep
