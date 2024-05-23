4d316a7efefdbba451f74ffc11895259c9a003a3
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-23 22:12:00 +0600

    login pages aeede

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import type { NextPage } from "next";
+import Head from "next/head";
+import LoginForm from "@/components/authUser/auth/LoginForm";
 
-const Login = () => {
+const LoginPage: NextPage = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Head>
+        <title>Login Page</title>
+        <meta
+          name="description"
+          content="Login page with Next.js, TypeScript, and TailwindCSS"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <LoginForm />
+    </div>
+  );
+};
 
-export default Login
+export default LoginPage;
